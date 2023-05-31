7e65e4323a0f248b503ab478f8893a69153139c9
IvanVulovic <ivn.vulovic@gmail.com>
2023-05-31 22:19:18 +0200

    Reducing gaps

--- sites/web/src/core/redux/IRootState.ts
+++ sites/web/src/core/redux/IRootState.ts
@@ -1,5 +1,3 @@
-import { CHAT_SCOPE } from "@web/pages/Home/constants";
-import { ChatState } from "@web/pages/Home/types";
 import { THEME_SCOPE } from "@web/providers/Theme/slice/constants";
 import { IThemeState } from "@web/providers/Theme/slice/types";
 
@@ -7,5 +5,4 @@ export interface IRootState {
   normalKey: any;
   persistedKey: any;
   [THEME_SCOPE]: IThemeState;
-  [CHAT_SCOPE]: ChatState;
 }
